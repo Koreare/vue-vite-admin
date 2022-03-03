3f498bf33965e2dac1dd6ccce386b85e1e8b1c17
zyh <624356793@qq.com>
2022-03-03 15:33:36 +0800

    Default Changelist

--- src/router/async-router.js
+++ src/router/async-router.js
@@ -5,12 +5,12 @@ const route = [
     path: '/',
     component: Layout,
     redirect: '/dashboard',
-    meta: { title: 'dashboard', icon: 'el-icon-menu' },
+    meta: { title: 'dashboard',roles: ['F-0100'], icon: 'el-icon-menu' },
     children: [
       {
         path: 'dashboard',
         component: () => import('@/views/dashboard/index.vue'),
-        meta: { title: '首页', icon: 'Grid', hideClose: true }
+        meta: { title: '首页', icon: 'Grid', roles: ['F-0101'], hideClose: true }
       }
     ]
   },
@@ -18,16 +18,16 @@ const route = [
     path: '/demo',
     component: Layout,
     alwaysShow: true,
-    meta: { title: '页面', icon: 'Calendar' },
+    meta: { title: '页面', roles: ['F-0200'], icon: 'Calendar' },
     children: [
       {
         path: 'demo',
-        meta: { title: '私有示例' },
+        meta: { title: '私有示例', roles: ['F-0201'], },
         component: () => import('@/views/demo/index.vue')
       },
       {
         path: 'text',
-        meta: { title: '测试' },
+        meta: { title: '测试', roles: ['F-0202'], },
         component: () => import('@/views/text/index.vue')
       }
     ]
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import layout from '../layout/index.vue'
+import store from '../store'
 
 import asyncRouter from './async-router'
 import constantRouter from './constant-router'
@@ -9,7 +9,31 @@ const asyncRoutes = [
   ...constantRouter
 ]
 
-const routes = asyncRoutes
+// 判断在权限中存不存在
+function isExit(roles, powerRoles) {
+  return roles.some(item => powerRoles.includes(item))
+}
+
+function distributeRouter(routerList ,powerRoles) {
+  const data = []
+  routerList.forEach(item => {
+    if(item.meta && item.meta.roles) {
+      if(isExit(item.meta.roles, powerRoles)) {
+        if(item.children) {
+          item.children = distributeRouter(item.children, powerRoles)
+        }
+        data.push(item)
+      }
+    }
+  })
+  return data
+}
+
+// console.log(store.state.user.info)
+const powerRoles = ['F-0100','F-0101', 'F-0200', 'F-0201']
+
+
+const routes = distributeRouter(asyncRoutes, powerRoles)
 
 const router = createRouter({
   history: createWebHistory(),
